30dbf45fc77c11403131b769e0b0d4cee7169b2f
aravind <aravind.srec.661@gmail.com>
2024-11-09 04:39:36 -0500

    Third Commit

--- src/main/java/com/springbatch/demo/SpringBatchDemoApplication.java
+++ src/main/java/com/springbatch/demo/SpringBatchDemoApplication.java
@@ -2,8 +2,9 @@ package com.springbatch.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringBatchDemoApplication {
 
 	public static void main(String[] args) {
